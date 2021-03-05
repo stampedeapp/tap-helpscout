dc09cc933f5355119466058abbb07710617027ab
Pankaj Saini <pankajsaini0803@gmail.com>
2021-03-05 14:09:22 +0000

    added first draft of client_credentials flow

--- tap_helpscout/client.py
+++ tap_helpscout/client.py
@@ -5,23 +5,21 @@ import requests
 from singer import metrics
 from singer import utils
 
+
 class Server5xxError(Exception):
     pass
 
+
 class Server429Error(Exception):
     pass
+
 
 class HelpScoutClient:
 
     # pylint: disable=too-many-instance-attributes
     # Nine is reasonable in this case.
 
-    def __init__(self,
-                 config_path,
-                 client_id,
-                 client_secret,
-                 refresh_token,
-                 user_agent):
+    def __init__(self, config_path, client_id, client_secret, refresh_token, user_agent):
         self.__config_path = config_path
         self.__client_id = client_id
         self.__client_secret = client_secret
@@ -39,85 +37,91 @@ class HelpScoutClient:
     def __exit__(self, exception_type, exception_value, traceback):
         self.__session.close()
 
-    @backoff.on_exception(backoff.expo,
-                          Server5xxError,
-                          max_tries=5,
-                          factor=2)
+    @backoff.on_exception(backoff.expo, Server5xxError, max_tries=5, factor=2)
     def get_access_token(self):
         if self.__access_token is not None and self.__expires > datetime.utcnow():
             return
 
         headers = {}
         if self.__user_agent:
-            headers['User-Agent'] = self.__user_agent
+            headers["User-Agent"] = self.__user_agent
 
-        response = self.__session.post(
-            url='https://api.helpscout.net/v2/oauth2/token',
-            headers=headers,
-            data={
-                'grant_type': 'refresh_token',
-                'client_id': self.__client_id,
-                'client_secret': self.__client_secret,
-                'refresh_token': self.__refresh_token,
-            })
+        if self.__refresh_token is None or self.__refresh_token == "":
+            response = self.__session.post(
+                url="https://api.helpscout.net/v2/oauth2/token",
+                headers=headers,
+                data={
+                    "grant_type": "client_credentials",
+                    "client_id": self.__client_id,
+                    "client_secret": self.__client_secret,
+                },
+            )
+        else:
+            response = self.__session.post(
+                url="https://api.helpscout.net/v2/oauth2/token",
+                headers=headers,
+                data={
+                    "grant_type": "refresh_token",
+                    "client_id": self.__client_id,
+                    "client_secret": self.__client_secret,
+                    "refresh_token": self.__refresh_token,
+                },
+            )
 
         if response.status_code >= 500:
             raise Server5xxError()
 
         if response.status_code != 200:
             helpscout_response = response.json()
-            helpscout_response.update(
-                {'status': response.status_code})
+            helpscout_response.update({"status": response.status_code})
             raise Exception(
-                'Unable to authenticate (HelpScout response: `{}`)'.format(
-                    helpscout_response))
+                "Unable to authenticate (HelpScout response: `{}`)".format(helpscout_response)
+            )
 
         data = response.json()
 
-        self.__access_token = data['access_token']
-        self.__refresh_token = data['refresh_token']
+        self.__access_token = data["access_token"]
+        if data.get("refresh_token") is not None:
+            self.__refresh_token = data["refresh_token"]
+            ## refresh_token rotates on every reauth
+            with open(self.__config_path) as file:
+                config = json.load(file)
+            config["refresh_token"] = data["refresh_token"]
+            with open(self.__config_path, "w") as file:
+                json.dump(config, file, indent=2)
 
-        ## refresh_token rotates on every reauth
-        with open(self.__config_path) as file:
-            config = json.load(file)
-        config['refresh_token'] = data['refresh_token']
-        with open(self.__config_path, 'w') as file:
-            json.dump(config, file, indent=2)
-
-        expires_seconds = data['expires_in'] - 60 # pad by 60 seconds
+        expires_seconds = data["expires_in"] - 60  # pad by 60 seconds
         self.__expires = datetime.utcnow() + timedelta(seconds=expires_seconds)
 
-
-    @backoff.on_exception(backoff.expo,
-                          (Server5xxError, ConnectionError, Server429Error),
-                          max_tries=7,
-                          factor=3)
+    @backoff.on_exception(
+        backoff.expo, (Server5xxError, ConnectionError, Server429Error), max_tries=7, factor=3
+    )
     @utils.ratelimit(400, 60)
     def request(self, method, path=None, url=None, **kwargs):
 
         self.get_access_token()
 
         if not url and self.__base_url is None:
-            self.__base_url = 'https://api.helpscout.net/v2'
+            self.__base_url = "https://api.helpscout.net/v2"
 
         if not url and path:
             url = self.__base_url + path
 
-        if 'endpoint' in kwargs:
-            endpoint = kwargs['endpoint']
-            del kwargs['endpoint']
+        if "endpoint" in kwargs:
+            endpoint = kwargs["endpoint"]
+            del kwargs["endpoint"]
         else:
             endpoint = None
 
-        if 'headers' not in kwargs:
-            kwargs['headers'] = {}
-        kwargs['headers']['Authorization'] = 'Bearer {}'.format(self.__access_token)
+        if "headers" not in kwargs:
+            kwargs["headers"] = {}
+        kwargs["headers"]["Authorization"] = "Bearer {}".format(self.__access_token)
 
         if self.__user_agent:
-            kwargs['headers']['User-Agent'] = self.__user_agent
+            kwargs["headers"]["User-Agent"] = self.__user_agent
 
-        if method == 'POST':
-            kwargs['headers']['Content-Type'] = 'application/json'
+        if method == "POST":
+            kwargs["headers"]["Content-Type"] = "application/json"
 
         with metrics.http_request_timer(endpoint) as timer:
             response = self.__session.request(method, url, **kwargs)
@@ -126,8 +130,8 @@ class HelpScoutClient:
         if response.status_code >= 500:
             raise Server5xxError()
 
-        #Use retry functionality in backoff to wait and retry if
-        #response code equals 429 because rate limit has been exceeded
+        # Use retry functionality in backoff to wait and retry if
+        # response code equals 429 because rate limit has been exceeded
         if response.status_code == 429:
             raise Server429Error()
 
@@ -136,7 +140,7 @@ class HelpScoutClient:
         return response.json()
 
     def get(self, path, **kwargs):
-        return self.request('GET', path=path, **kwargs)
+        return self.request("GET", path=path, **kwargs)
 
     def post(self, path, **kwargs):
-        return self.request('POST', path=path, **kwargs)
+        return self.request("POST", path=path, **kwargs)
